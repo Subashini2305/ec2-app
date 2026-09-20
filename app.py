08b5f88b7e70d683b7e4ca32da5dbb137489381b
Subashini2305 <subasrinivasan2005@gmail.com>
2026-09-20 15:16:30 +0530

    test commit

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def home():
     return """
     <h1>Hello from AWS EC2! 🚀</h1>
-    <p>My Python Flask application is running on an EC2 instance.</p>
+    <p>testtttt My Python Flask application is running on an EC2 instance.hi </p>
     """
 
 @app.route("/about")
